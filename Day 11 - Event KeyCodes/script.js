4b90e0f72463cdbe9e7105cb77ead4a950acb936
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-04 19:01:58 +0900

    Added script

--- Day 11 - Event KeyCodes/script.js
+++ Day 11 - Event KeyCodes/script.js
@@ -1,1 +1,20 @@
+const insert = document.getElementById("insert");
 
+window.addEventListener("keydown", (event) => {
+  insert.innerHtml = `
+    <div class="key">
+    ${event.key === " " ? "Space" : event.key}
+    <small>event.key</small>
+  </div>
+
+  <div class="key">
+    ${event.keyCode}
+    <small>event.keycode</small>
+  </div>
+
+  <div class="key">
+    ${event.code}
+    <small>event.code</small>
+  </div>  
+    `;
+});
