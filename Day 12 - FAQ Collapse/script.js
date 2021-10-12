b4b8c70fd0c4ff989276a6e535c4526a02a64422
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-12 15:19:46 +0900

    added script

--- Day 12 - FAQ Collapse/script.js
+++ Day 12 - FAQ Collapse/script.js
@@ -1,1 +1,14 @@
+// Bring in toggle buttons (querySelectorAll)
+const toggles = document.querySelectorAll(".faq-toggle");
 
+// Loop through nodelist (forEach)
+
+toggles.forEach((toggle) => {
+  // Add click event(addEventListener)
+
+  toggle.addEventListener("click", () => {
+    // Toggle the active class on the parent node(.parentNode & classList.toggle())
+
+    toggle.parentNode.classList.toggle("active");
+  });
+});
